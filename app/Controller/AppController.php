2ae83cdbe1c7337ff593a640ce5f05ad0bdf75ec
Mario Francisco <marioasfrancisco@gmail.com>
2014-10-21 16:59:32 +0100

    Adicionado suporte ao plugin:  BoostCake, this is a plugin for CakePHP using Bootstrap Instalação do Composer, que faz a gestão e instalação de plugins

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,62 +1,78 @@
-<?php
-/**
- * Application level Controller
- *
- * This file is application-wide controller file. You can put all
- * application-wide controller-related methods here.
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Controller
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
+    <?php
+    /**
+     * Application level Controller
+     *
+     * This file is application-wide controller file. You can put all
+     * application-wide controller-related methods here.
+     *
+     * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+     * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+     *
+     * Licensed under The MIT License
+     * For full copyright and license information, please see the LICENSE.txt
+     * Redistributions of files must retain the above copyright notice.
+     *
+     * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+     * @link          http://cakephp.org CakePHP(tm) Project
+     * @package       app.Controller
+     * @since         CakePHP(tm) v 0.2.9
+     * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+     */
 
-App::uses('Controller', 'Controller');
+    App::uses('Controller', 'Controller');
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package		app.Controller
- * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
- */
-class AppController extends Controller {
+    /**
+     * Application Controller
+     *
+     * Add your application-wide methods in the class below, your controllers
+     * will inherit them.
+     *
+     * @package		app.Controller
+     * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+     */
+    class AppController extends Controller {
 
-    public $theme = 'Bracket';
+        public $theme = 'Bracket';
 
-    public $components = array(
-        'Session',
-        'Auth' => array(
-            'loginRedirect' => array(
-                'controller' => 'posts',
-                'action' => 'index'
-            ),
-            'logoutRedirect' => array(
-                'controller' => 'users',
-                'action' => 'login'
-            ),
-            'authenticate' => array(
-                'Form' => array(
-                    'passwordHasher' => 'Blowfish'
+        public $helpers = array(
+            'Session',
+            'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
+            'Form' => array('className' => 'BoostCake.BoostCakeForm'),
+            'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
+        );
+ 
+
+        public $components = array(
+            'Session',
+            'Auth' => array(
+                'loginRedirect' => array(
+                    'controller' => 'posts',
+                    'action' => 'index'
+                ),
+                'logoutRedirect' => array(
+                    'controller' => 'users',
+                    'action' => 'login'
+                ),
+                'authenticate' => array(
+                    'Form' => array(
+                        'passwordHasher' => 'Blowfish'
+                    )
+                ),
+                 'flash' => array(
+                    'element' => 'alert',
+                    'key' => 'auth',
+                    'params' => array(
+                        'plugin' => 'BoostCake',
+                        'class' => 'alert-error'
+                    )
                 )
             )
-        )
-    );
- 
-// only allow the login controllers only
-public function beforeFilter() {
-    $this->Auth->allow('login');
-    //$this->Auth->allow('signup');
-}
- 
-}
+        );
+     
+    // only allow the login controllers only
+    public function beforeFilter() {
+        $this->Auth->allow('login');
+        //$this->Auth->allow('signup');
+    }
+     
+    }
